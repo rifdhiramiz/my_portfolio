360158ec5ae003eddaf75d61a581cbd45e392abe
rifdhiramiz <rifdhiramiz18@gmail.com>
2025-11-15 08:44:25 +0530

    Switch from BrowserRouter to HashRouter to fix 404 on static hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
